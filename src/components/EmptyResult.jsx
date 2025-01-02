78e1a576284d94c7a7c6e35802cc02135364e52e
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-01-02 19:02:03 +0200

    Update EmptyResult.jsx

--- src/components/EmptyResult.jsx
+++ src/components/EmptyResult.jsx
@@ -1,5 +1,14 @@
 function EmptyResult() {
-
+  return (
+    <div className="empty-state">
+      <img src="./illustration-empty.svg" />
+      <h2>Results shown here</h2>
+      <p>
+        Complete the form and click "calculate repayments" to see what your
+        monthly payments would be.
+      </p>
+    </div>
+  );
 }
 
 export default EmptyResult;
