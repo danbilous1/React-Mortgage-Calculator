c11cfe23e1d5e5979a9d95f8b69c98b301c09f97
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-01-03 23:31:10 +0200

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,27 @@
 function Input({ label, id, inputLabel, position, value, onChange, type }) {
+  return (
+    <div className="input-container">
+      <label style={{ margin: "5px", fontSize: "16px" }} htmlFor={id}>
+        {label}
+      </label>
+      <div
+        style={{ flexDirection: position == "right" ? "row" : "row-reverse" }}
+        className="input-row"
+      >
+        <input
+          id={id}
+          value={value}
+          onChange={onChange}
+          type={type}
+          onInput={onChange}
+        />
+        <div>
+          <p style={{ margin: "5px" }}>{inputLabel}</p>
+        </div>
+      </div>
+      <div className="field none">This field is required</div>
+    </div>
+  );
 }
 
 export default Input;
