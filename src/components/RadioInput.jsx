ec6722e93251e1d38cff49a767507061925f9905
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2025-01-03 23:31:37 +0200

    Update RadioInput.jsx

--- src/components/RadioInput.jsx
+++ src/components/RadioInput.jsx
@@ -1,1 +1,17 @@
+function RadioInput({ label, id, value, onChange }) {
+  return (
+    <div>
+      <input
+        type="radio"
+        value={value}
+        id={id}
+        name="drone"
+        onChange={onChange}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+}
+
+export default RadioInput;
 
